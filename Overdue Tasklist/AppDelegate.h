c51f1cf88b6d365198e8834e225e5a645ea32c06
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-28 09:46:16 +1000

    Initial Commit

---
+++ Overdue Tasklist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Overdue Tasklist
+//
+//  Created by Gauthier Delmee on 28/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
