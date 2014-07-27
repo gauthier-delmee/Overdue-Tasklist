e79cf6035444bc2438e9cc6b43f868e8ed947a21
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-28 09:56:03 +1000

    Added view controllers (AddTask, DetailTask and EditTask) and added segues for navigation

---
+++ Overdue Tasklist/DetailTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTaskViewController.h
+//  Overdue Tasklist
+//
+//  Created by Gauthier Delmee on 28/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTaskViewController : UIViewController
+
+@end
---
+++ Overdue Tasklist/EditTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditTaskViewController.h
+//  Overdue Tasklist
+//
+//  Created by Gauthier Delmee on 28/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditTaskViewController : UIViewController
+
+@end
