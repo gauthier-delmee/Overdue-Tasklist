4eb03f6a0321d3e775c727d131516484809cdbc6
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-29 18:56:51 +1000

    color coding UITableView cells based on task being overdue and added functionality to delete tasks from the application

--- Overdue Tasklist/ViewController.m
+++ Overdue Tasklist/ViewController.m
@@ -97,6 +97,17 @@
     return taskObject;
 }
 
+-(BOOL)isDateGreaterThanDate:(NSDate *)date and:(NSDate *)toDate{
+    int timeIntervalForDate = [date timeIntervalSince1970];
+    int timeIntervalForToDate = [toDate timeIntervalSince1970];
+    
+    if (timeIntervalForDate < timeIntervalForToDate) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
 #pragma mark - UITableViewDataSource Methods
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return [self.taskObjects count];
@@ -114,11 +125,38 @@
     cell.textLabel.text = task.title;
     
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"yyyy-mm-dd"];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
     NSString *stringFromDate = [formatter stringFromDate:task.date];
     cell.detailTextLabel.text = stringFromDate;
     
+    if ([self isDateGreaterThanDate:task.date and:[NSDate date]]) {
+        cell.backgroundColor = [UIColor yellowColor];
+    } else {
+        cell.backgroundColor = [UIColor redColor];
+    }
+    
     return cell;
+}
+
+#pragma mark - UITableViewDelegate Methods
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        //delete the row from the data source
+        [self.taskObjects removeObjectAtIndex:indexPath.row];
+        
+        NSMutableArray *newSavedTaskObjectsData = [[NSMutableArray alloc] init];
+        for (Task *taskObject in self.taskObjects) {
+            [newSavedTaskObjectsData addObject:[self taskObjectsAsAPropertyList:taskObject]];
+        }
+        [[NSUserDefaults standardUserDefaults] setObject:newSavedTaskObjectsData forKey:ADDED_TASKS_OBJECTS_KEY];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
 }
 
 
